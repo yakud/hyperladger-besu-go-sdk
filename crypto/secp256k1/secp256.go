48066be6c811a51a28e3e9429b1b78325e91547a
yakud <yakudgm@gmail.com>
2022-03-21 00:53:38 +0100

    Remove secp256 for test

--- crypto/secp256k1/secp256.go
+++ crypto/secp256k1/secp256.go
@@ -26,13 +26,14 @@ package secp256k1
 #define USE_FIELD_INV_BUILTIN
 #define USE_SCALAR_INV_BUILTIN
 #define NDEBUG
-#include "./libsecp256k1/src/secp256k1.c"
-#include "./libsecp256k1/src/modules/recovery/main_impl.h"
-#include "ext.h"
 
-typedef void (*callbackFunc) (const char* msg, void* data);
-extern void secp256k1GoPanicIllegal(const char* msg, void* data);
-extern void secp256k1GoPanicError(const char* msg, void* data);
+//#include "./libsecp256k1/src/secp256k1.c"
+//#include "./libsecp256k1/src/modules/recovery/main_impl.h"
+//#include "ext.h"
+
+//typedef void (*callbackFunc) (const char* msg, void* data);
+//extern void secp256k1GoPanicIllegal(const char* msg, void* data);
+//extern void secp256k1GoPanicError(const char* msg, void* data);
 */
 import "C"
 
